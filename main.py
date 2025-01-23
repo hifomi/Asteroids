c5cd574e5d8a765e3c0459e10d3630ecf270c444
hifomi <hiiifomi@gmail.com>
2025-01-22 18:25:18 -0800

    sprites, player, movement, rotation

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
-def initialize_game():
+
+def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    return screen, clock
-
-def handle_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return True
-    return False
-
-def update_screen(screen):
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
-
-def main():
-    screen, clock = initialize_game()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
-    while True:  # Main game loop
-        if handle_events():  # Check for quit
-            break
-        
-        update_screen(screen)  # Draw the screen
-        
-        # Get dt in seconds (divide by 1000 to convert from milliseconds)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
